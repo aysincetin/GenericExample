8ca9b030c0d41bfdec35f53ca25b9024f764f996
Ayşin <aysincetinn@hotmail.com>
2021-01-23 19:35:34 +0300

    ASCII Codes

--- GenericExample/MyDirectory.cs
+++ GenericExample/MyDirectory.cs
@@ -10,33 +10,33 @@ namespace GenericExample
         T[] numberArray;
         T[] tempNumberArray;
 
-        K[] nameArray;
-        K[] tempNameArray;
+        K[] codeArray;
+        K[] tempcodeArray;
 
         public MyDirectory()
         {
             numberArray = new T[0];
-            nameArray = new K[0];
+            codeArray = new K[0];
         }
-        public void Add(T number, K name)
+        public void Add(T number, K code)
         {
             tempNumberArray = numberArray;
             numberArray = new T[numberArray.Length + 1];
 
-            tempNameArray = nameArray;
-            nameArray = new K[nameArray.Length + 1];
+            tempcodeArray = codeArray;
+            codeArray = new K[codeArray.Length + 1];
 
             for (int i = 0; i < tempNumberArray.Length; i++)
             {
-                for (int j = 0; j < tempNameArray.Length; j++)
+                for (int j = 0; j < tempcodeArray.Length; j++)
                 {
 
-                    nameArray[j] = tempNameArray[j];
+                    codeArray[j] = tempcodeArray[j];
                 }
                 numberArray[i] = tempNumberArray[i];
             }
             numberArray[numberArray.Length - 1] = number;
-            nameArray[nameArray.Length - 1] = name;
+            codeArray[codeArray.Length - 1] = code;
         }
         public int Count
         {
@@ -53,7 +53,7 @@ namespace GenericExample
             Console.WriteLine("---ASCII Codes---");
             for (int i = 0; i < numberArray.Length; i++)
             {
-                Console.WriteLine(numberArray[i] + " - " + nameArray[i]);
+                Console.WriteLine(numberArray[i] + " - " + codeArray[i]);
             }
         }
     }
